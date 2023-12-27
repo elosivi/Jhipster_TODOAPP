4e19eb973ac7ba74b1301f018983e0ef0fb60f8f
0ebarbe <ebarbe@aubay.com>
2023-12-27 18:32:10 +0100

    [test][entitie person] - maj

--- src/test/java/com/ebarbe/domain/PersonTestSamples.java
+++ src/test/java/com/ebarbe/domain/PersonTestSamples.java
@@ -10,14 +10,18 @@ public class PersonTestSamples {
     private static final AtomicLong longCount = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
 
     public static Person getPersonSample1() {
-        return new Person().id(1L).description("description1");
+        return new Person().id(1L).description("description1").pseudo("pseudo1").name("name1");
     }
 
     public static Person getPersonSample2() {
-        return new Person().id(2L).description("description2");
+        return new Person().id(2L).description("description2").pseudo("pseudo2").name("name2");
     }
 
     public static Person getPersonRandomSampleGenerator() {
-        return new Person().id(longCount.incrementAndGet()).description(UUID.randomUUID().toString());
+        return new Person()
+            .id(longCount.incrementAndGet())
+            .description(UUID.randomUUID().toString())
+            .pseudo(UUID.randomUUID().toString())
+            .name(UUID.randomUUID().toString());
     }
 }
